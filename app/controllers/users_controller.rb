dd4fb4c889bd846b209fe4ffe9bac13deba0301a
maimux2x <m0smile10often@gmail.com>
2022-08-21 15:31:10 +0900

    users_controllerとviewの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
