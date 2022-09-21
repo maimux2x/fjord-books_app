4b932a89e3b4f696cd271c4e4487c5fbf032b8c7
maimux2x <m0smile10often@gmail.com>
2022-09-21 22:35:49 +0900

    レビューいただいた内容の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png] },
-                      size: { less_than: 1.megabytes }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png] }, size: { less_than: 1.megabytes }
 end
