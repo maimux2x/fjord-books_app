07ed1008e33ab79c4196f0a000cc43f5deb5dc6c
maimux2x <m0smile10often@gmail.com>
2022-09-25 13:49:40 +0900

    UserモデルとReportモデルのテストを書いた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:alice)
+    @report = reports(:one)
+    @other_report = reports(:two)
+  end
+  
+  test "user_should_edit_own_report" do
+    assert_not @other_report.editable?(@user)
+    assert @report.editable?(@user)
+  end
+
+  test "created_at_should_convert_date" do
+    assert @report.created_on
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,31 @@
-# frozen_string_literal: true
-
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:alice)
+    @other_user = users(:bob)
+  end
+  test "user_should_have_email_or_name" do
+    test_user = User.new(email: 'test@example.com', name: '')
+    assert_equal 'test@example.com', test_user.name_or_email
+    
+    test_user.name = ' Test Test'
+    assert_equal = 'Test Test', test_user.name_or_email
+  end
+  
+  test "user_should_follow_otoher_user" do
+    assert_not @user.following?(@other_user)
+    @user.follow(@other_user)
+    assert @user.following?(@other_user)
+  end
+  
+  test "user_should_unfollow_otoher_user" do
+    @user.unfollow(@other_user)
+    assert_not @user.following?(@other_user)
+  end
+
+  test "user_should_be_followed_other_user" do
+    @other_user.follow(@user)
+    assert @user.followed_by?(@other_user)
+  end
 end
