c0ec0849025166bf2521362d3094f97ab782617f
maimux2x <m0smile10often@gmail.com>
2022-10-09 19:52:59 +0900

    本と日報のCRUD部分のテストとmodelのテストを実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,13 +8,13 @@ class ReportTest < ActiveSupport::TestCase
     @report = reports(:one)
     @other_report = reports(:two)
   end
-  
-  test "user_should_edit_own_report" do
+
+  test 'user_should_edit_own_report' do
     assert_not @other_report.editable?(@user)
     assert @report.editable?(@user)
   end
 
-  test "created_at_should_convert_date" do
+  test 'created_at_should_convert_date' do
     assert @report.created_on
   end
 end
